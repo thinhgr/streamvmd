e9cf7478fe5ed76926add9bc13f02d360e01c127
thinhgr <thinhvemedim@gmail.com>
2018-02-26 10:16:01 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
 socket.on('DANG_KY_THAT_BAI', () => alert('Vui long chon username khac'))
 
 function openStream() {
-    const config = { audio: false, video: true }
+    const config = { audio: true, video: true }
     return navigator.mediaDevices.getUserMedia(config)
 }
 
